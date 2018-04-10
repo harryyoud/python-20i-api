02f38965394176d790d59e2c00885aa029f7e5b7
Harry Youd <harry@harryyoud.co.uk>
2018-04-10 11:44:05 +0100

    Add docstrings

--- twentyi_api/api.py
+++ twentyi_api/api.py
@@ -5,6 +5,26 @@ import base64
 class TwentyIRestAPI:
     def __init__(self, auth_url="https://auth-api.20i.com:3000",
                  url="https://api.20i.com", auth=None):
+        """Process and store token used for authenticating to the REST API
+
+        Keyword arguments:
+         - str(auth_url) = the URL used for subuser authentication. defaults to
+                      https://auth-api.20i.com:3000 if not given
+         - str(url) = the URL used for the REST API commands. defaults to
+                      https://api.20i.com if not given
+         - dict(auth) = only bearer being set results in reseller level access
+                        both bearer and username being set results in scope
+                        limited to subuser access
+                        bearer, username and password being set results in
+                        scope being limited to subuser access
+                        {
+                          "bearer": reseller_token
+                          "username": (optional) subuser_username
+                          "password": (optional) subuser_password
+                        }
+        """
+
+        # We add this ourselves, remove it now to prevent double slash
         self.url = url.rstrip('/')
 
         if isinstance(auth, dict):
@@ -56,15 +76,30 @@ class TwentyIRestAPI:
             raise ValueError("Please supply authentication")
 
     def token_to_bearer(self, token):
+        """base64 encode given token for use in HTTP headers
+
+        Keyword arguments:
+         - str(token) - Usually looks something like c6d7b42a36fcd2625
+        """
         token = token.encode()
         token = base64.b64encode(token)
         token = token.decode()
         return token
 
     def get_url(self, endpoint):
+        """Return URL from combining api URL and endpoint, stripping slashes
+
+        Keyword arguments:
+         - str(endpoint) - looks something like "/package/example.com/domain"
+        """
         return self.url + '/' + endpoint.lstrip('/')
 
     def decode_response(self, response):
+        """Extract JSON from response or return an error if unable
+
+        Keyword arguments:
+         - response - requests module response type
+        """
         error = None
         try:
             jr = response.json()
@@ -81,6 +116,12 @@ class TwentyIRestAPI:
         return jr
 
     def get(self, endpoint=None, **kwargs):
+        """GET the endpoint and return the response as JSON if decodable
+
+        Keyword arguments:
+         - str(endpoint) - Looks something like "/package/example.com/domain"
+         - **kwargs - all other keyword args are passed to requests.get
+        """
         if endpoint is None:
             raise ValueError("no URL supplied")
         headers = {"Authorization": "Bearer {}".format(self.auth)}
@@ -88,6 +129,14 @@ class TwentyIRestAPI:
         return self.decode_response(r)
 
     def post(self, endpoint=None, data=None, **kwargs):
+        """POST the endpoint with given data (if supplied) and return the
+        response as a dict if JSON-decodable
+
+        Keyword arguments:
+         - str(endpoint) - Looks something like "/domain/example.com/dns"
+         - dict(data) - JSON body sent to server
+         - **kwargs - all other keyword args are passed to requests.post
+        """
         if endpoint is None:
             raise ValueError("no URL supplied")
         headers = {"Authorization": "Bearer {}".format(self.auth)}
