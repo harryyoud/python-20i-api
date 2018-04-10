a8cd2c79377fb1a92b5c6ad958fe8d5f364108ef
Harry Youd <harry@harryyoud.co.uk>
2018-04-10 08:54:34 +0100

    Wrap to 79 chars per PEP8

--- twentyi_api/api.py
+++ twentyi_api/api.py
@@ -3,26 +3,34 @@ import json
 import base64
 
 class TwentyIRestAPI:
-    def __init__(self, auth_url="https://auth-api.20i.com:3000", url="https://api.20i.com", auth=None):
+    def __init__(self, auth_url="https://auth-api.20i.com:3000",
+                 url="https://api.20i.com", auth=None):
         self.url = url.rstrip('/')
 
         if isinstance(auth, dict):
-            # Use reseller token, subuser username and subuser password to limit to subuser scope
             if "bearer" in auth and "username" in auth and "password" in auth:
+                """Use reseller token, subuser username and subuser password to
+                limit to subuser scope"""
                 headers = {"Authorization": "Bearer {}".format(auth["bearer"])}
-                o = {"grant_type": "password", "username": auth["username"], "password": auth["password"]}
-                r = requests.post(auth_url+"/login/authenticate", json=o, headers=headers)
+                o = {"grant_type": "password", "username": auth["username"],
+                     "password": auth["password"]}
+                r = requests.post(auth_url+"/login/authenticate", json=o,
+                                  headers=headers)
                 try:
                     token = self.token_to_bearer(r.json()["access_token"])
                     self.auth = token
                 except ValueError as e:
-                    raise Exception("Got unexpected response from login endpoint") from e
-            # Use reseller token and subuser username to limit to subuser scope
+                    raise Exception("Got unexpected response from login "
+                                    "endpoint") from e
             elif "bearer" in auth and "username" in auth:
+                """Use reseller token and subuser username to limit to subuser
+                scope"""
                 headers = {"Authorization": "Bearer {}".format(auth["bearer"])}
                 r = []
-                r.append(requests.get(auth_url+"/user/stack-user", headers=headers).json())
-                r.append(requests.get(auth_url+"/user/service-user", headers=headers).json())
+                r.append(requests.get(auth_url+"/user/stack-user",
+                                      headers=headers).json())
+                r.append(requests.get(auth_url+"/user/service-user",
+                                      headers=headers).json())
                 username = None
                 for user in r[0]:
                     if auth["username"] == user["name"]:
@@ -30,14 +38,16 @@ class TwentyIRestAPI:
                 if subuser_scope is None:
                     raise Exception("Username not found in user list")
                 o = {"grant_type": "client_credentials", "scope": subuser_scope}
-                r = requests.post(auth_url+"/login/authenticate", json=o, headers=headers)
+                r = requests.post(auth_url+"/login/authenticate", json=o,
+                                  headers=headers)
                 try:
                     token = self.token_to_bearer(r.json()["access_token"])
                     self.auth = token
                 except ValueError as e:
-                    raise Exception("Got unexpected response from login endpoint") from e
-            # Use reseller token
+                    raise Exception("Got unexpected response from login "
+                                    "endpoint") from e
             elif "bearer" in auth:
+                """Use reseller token"""
                 token = self.token_to_bearer(auth["bearer"])
                 self.auth = token
             else:
@@ -64,7 +74,8 @@ class TwentyIRestAPI:
             except:
                 pass
         except json.decoder.JSONDecodeError as e:
-            raise ValueError("No valid JSON was returned from the server") from e
+            raise ValueError("No valid JSON was returned from the"
+                             "server") from e
         if error is not None:
             print(json.dumps(error, indent=2))
         response.raise_for_status()
@@ -81,5 +92,6 @@ class TwentyIRestAPI:
         if endpoint is None:
             raise ValueError("no URL supplied")
         headers = {"Authorization": "Bearer {}".format(self.auth)}
-        r = requests.post(self.get_url(endpoint), json=data, **kwargs, headers=headers)
+        r = requests.post(self.get_url(endpoint), json=data, **kwargs,
+                          headers=headers)
         return self.decode_response(r)
